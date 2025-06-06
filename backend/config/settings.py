c4dc45241ef9accd7ceae6a822a21dc8210fb9d4
Hanseoyun312 <hansy68922@naver.com>
2025-06-07 02:37:46 +0900

    중간 점검

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -45,9 +45,11 @@ INSTALLED_APPS = [
     'chatbot',
     'ranking_members',
     'ranking_parties',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -137,3 +139,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ALLOWED_ORIGINS = [
+    "https://osbasicproject-4.vercel.app",
+]
