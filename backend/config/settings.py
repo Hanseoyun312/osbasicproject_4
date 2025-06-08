f3555fe078ab8c748052792fe7d08e895297c9d6
Hanseoyun312 <hansy68922@naver.com>
2025-06-09 01:15:48 +0900

    wpqkf

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -147,9 +147,11 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     "https://osbasicproject-4.vercel.app",
+    "https://osbasicproject-4-pink.vercel.app/"
     
 ]
 
