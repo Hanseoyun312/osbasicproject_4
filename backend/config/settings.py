7ebc19fcc10ca000fa86d2399e5a00244edc1919
Hanseoyun312 <hansy68922@naver.com>
2025-06-10 21:23:46 +0900

    챗봇

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
     'ranking_members',
     'ranking_parties',
     'corsheaders',
-    'apply_weights',
     'pybo.apps.PyboConfig', # 여기서부터 추가
     'legislation.apps.LegislationConfig',
     'attendance.apps.AttendanceConfig',
